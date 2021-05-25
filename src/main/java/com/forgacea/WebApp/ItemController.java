2daec99041ca6eb6e22e72cd02d3d7ec99c6a84d
purice <PuriceL.Claudiu@gmail.com>
2021-05-25 10:21:20 +0300

    Add logging
    for the 3 existing methods

--- src/main/java/com/forgacea/WebApp/ItemController.java
+++ src/main/java/com/forgacea/WebApp/ItemController.java
@@ -1,5 +1,7 @@
 package com.forgacea.WebApp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,10 @@ import java.util.List;
 
 @RestController
 public class ItemController {
+	private static final Logger logger = LoggerFactory.getLogger(ItemController.class);
 	@GetMapping("items")
 	List<Item> getItems(){
+		logger.info("get Items called");
 		return new ArrayList<>();
 	}
 
--- src/main/java/com/forgacea/WebApp/StaticController.java
+++ src/main/java/com/forgacea/WebApp/StaticController.java
@@ -1,5 +1,7 @@
 package com.forgacea.WebApp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,13 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class StaticController {
+	private static final Logger logger = LoggerFactory.getLogger(StaticController.class);
 
 	@GetMapping("loginPage")
 	public String getLogin() {
+		logger.info("login page accessed");
 		return "loginPage.html";
 	}
 	@GetMapping({"", "index"})
 	public String getIndex() {
+		logger.info("index accessed");
 		return "index.html";
 	}
 }
